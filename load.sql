ae3177ba64d3d26d38c62c1cd5b1466782bf4091
Raul Jimenez <officialrauljimenez@gmail.com>
2019-04-25 12:26:07 -0500

    added initialization data

---
+++ load.sql
@@ -0,0 +1,188 @@
+INSERT INTO PROFILE (Email, Phone, Fname, Lname, Minit, Jdate, P_type, P_Password, Uname) VALUES
+('gospodin@live.com', '2312959984', 'Macbeth', 'Trapani', 'N', '2019-01-14 07:17:48', 'u', 'QWnwJP8ctVP', 'Archeyan'),
+('boomzilla@optonline.net', '9602396941', 'Abhishek', 'Moore', 'A', '2019-03-30 09:13:44', 'u', 'du6wLzfFzzq', 'Arnalymu'),
+('eurohack@yahoo.ca', '8484698211', 'Marva', 'McDaniel', 'J', '2019-02-15 19:15:32', 'u', 'FRt4XSBk79C', 'Bioherm'),
+('oneiros@msn.com', '7366445551', 'Yasmin', 'Thorpe', 'P', '2019-02-22 02:20:20', 'u', 'y87Gk5vDG7L', 'BlabBandit'),
+('lstaf@mac.com', '9576388302', 'Felicidad', 'Anthonyson', 'I', '2019-01-13 03:06:08', 'u', '9HgGUPydm8K', 'Blondcitype'),
+('tmccarth@att.net', '8473702170', 'Mikael', 'Egger', 'L', '2019-03-22 07:35:14', 'u', 'Vky6jEd5kZw', 'Bonviles'),
+('ramollin@verizon.net', '5635783030', 'Quetzalcoatl', 'McCaig', 'M', '2019-02-21 11:39:47', 'u', 'LAVBt4T2ep7', 'Burkeover'),
+('pavel@mac.com', '9573960578', 'Baruch', 'Grier', 'P', '2019-04-02 17:43:55', 'u', '3ERZgeJk4WZ', 'Canyonsor'),
+('cyrus@yahoo.ca', '4215252137', 'Mansur', 'Cruz', 'K', '2019-02-06 22:34:36', 'u', 'xAU3AAHzrT6', 'Cheenobusk'),
+('danny@aol.com', '5139157569', 'Sigge', 'Sauvage', 'H', '2019-01-11 17:56:15', 'u', 'PHFg7Amktn8', 'Coverageld'),
+('sinkou@att.net', '8367608571', 'Ryouta', 'Melville', 'G', '2019-02-13 20:34:38', 'u', 'PQ6hqQPFXMn', 'Crayonicom'),
+('lcheng@att.net', '9406871345', 'Alfbern', 'Brotz', 'H', '2019-03-09 20:27:46', 'u', 'n5fBasnBUMZ', 'Forgeridi'),
+('skaufman@hotmail.com', '7367840283', 'Frediano', 'Sharma', 'P', '2019-03-09 04:50:47', 'u', 'ZVxAt2dgwEv', 'KhadNote'),
+('stakasa@icloud.com', '3498674399', 'Theocritus', 'Alessi', 'C', '2019-03-25 19:35:01', 'u', 'SKzrjc6Wcm7', 'KinAut'),
+('burns@outlook.com', '5212705562', 'Eilwen', 'Eason', 'C', '2019-02-24 01:56:41', 'u', 'b8kpRLRkcCf', 'Litechbl'),
+('tlinden@yahoo.ca', '8246310116', 'Murchadh', 'Mueller', 'N', '2019-03-09 22:52:54', 'u', 'JcQQ9JqCAzq', 'Merlante'),
+('amimojo@me.com', '6735903662', 'Dilipa', 'Chaudhari', 'L', '2019-01-31 02:51:57', 'u', 'bpSRyLTFygq', 'MomUpfor'),
+('smartfart@me.com', '9412914030', 'Remo', 'Rigby', 'N', '2019-04-04 00:53:38', 'u', 'QycRgdRy7CN', 'Narancybe'),
+('schumer@hotmail.com', '8567808408', 'Goda', 'Horne', 'T', '2019-02-12 05:58:46', 'u', '6QLU3kXE6ME', 'Number1in'),
+('jbuchana@verizon.net', '5213058466', 'Abena', 'Pletcher', 'M', '2019-03-21 04:41:45', 'u', 'ZrCyvVHpzne', 'NumGod'),
+('earmstro@hotmail.com', '2282031917', 'Haunani', 'Forsberg', 'N', '2019-03-03 17:41:26', 'u', 'w4wTGcJgLBt', 'Ocucommer'),
+('goresky@gmail.com', '2378625034', 'Eiran', 'Roncalli', 'Q', '2019-01-03 02:53:30', 'u', 'kucMxq2t9Jt', 'Orrhythe'),
+('miltchev@yahoo.ca', '4363913177', 'Lykourgos', 'Dunst', 'L', '2019-03-20 23:33:24', 'u', 'ceQ5ArNyUrS', 'Panterb'),
+('noahb@msn.com', '6492547776', 'Jam', 'MacShuibhne', 'T', '2019-02-06 00:37:12', 'u', 'KWUvsAPba2L', 'Pawnata'),
+('bmidd@mac.com', '9108340015', 'Madelyn', 'Josephson', 'T', '2019-01-10 17:18:08', 'u', '8d887bWSHKK', 'PeatearFoot'),
+('pdbaby@hotmail.com', '8909239737', 'Ahti', 'McNaughton', 'J', '2019-02-13 11:00:31', 'u', 'QeWvGSUpskN', 'PostFate'),
+('jkegl@yahoo.ca', '5898864247', 'Hector', 'Schroeder', 'S', '2019-01-13 09:27:33', 'u', 'cCRp5CvLRGh', 'Queyarmaid'),
+('kodeman@comcast.net', '7464385857', 'Roshanara', 'Seelenfreund', 'E', '2019-03-05 07:14:10', 'u', '8RQSMDD2bZh', 'ReporterXo'),
+('fairbank@mac.com', '5755480774', 'Iskandar', 'Severijns', 'M', '2019-01-11 15:50:53', 'u', 'Dkfs8xkZvFP', 'TickerChik'),
+('chaki@comcast.net', '6666905330', 'Augustine', 'Morgenstern', 'D', '2019-01-04 00:36:50', 'u', 'sC3nzT4ts5Y', 'TimeMatter'),
+('mlewan@comcast.net', '7553787701', 'Lalitha', 'Sarkozi', 'S', '2019-04-19 15:05:15', 'u', 't9ZJ353MQnH', 'Commpylea'),
+('amaranth@att.net', '3574001898', 'Cristi', 'Campos', 'G', '2019-01-27 22:34:19', 'u', 's2mnXfPcgng', 'Cowserde'),
+('dkrishna@gmail.com', '4389004612', 'Lorena', 'Coburn', 'B', '2019-03-02 16:58:16', 'u', 'Ef7GcNnUm9e', 'Detecifie'),
+('jamuir@sbcglobal.net', '5427051251', 'Bronislava', 'Abramo', 'N', '2019-02-01 07:42:11', 'u', 'WZhm7HnTTvw', 'DoggKhad'),
+('luvirini@att.net', '5954629116', 'Dominykas', 'Borisov', 'A', '2019-01-03 05:27:22', 'u', 'cM3zH6Y2N8G', 'Dysgnex'),
+('lstein@gmail.com', '9042665091', 'July', 'Havel', 'C', '2019-03-10 11:42:51', 'u', 'ZAfssZjGbpH', 'Emarment'),
+('carmena@msn.com', '5192881529', 'Karthikeyan', 'Lakatos', 'O', '2019-01-17 15:30:00', 'u', 'm8uh4rfUPvd', 'Ferdymedxch'),
+('stevelim@hotmail.com', '8098199476', 'Seve', 'Elzinga', 'L', '2019-03-10 06:24:37', 'u', '2EzreCM4DYZ', 'ForFreex'),
+('nichoj@msn.com', '7434568504', 'Bran', 'Kynaston', 'Q', '2019-03-20 14:43:36', 'u', 'xeVcU5KVgLR', 'Goofylkerca'),
+('aaribaud@mac.com', '4406241897', 'Petro', 'Abbingh', 'A', '2019-03-27 18:56:34', 'u', 'xqM8GspK2Pq', 'Guitaristem'),
+('ghost@me.com', '4866102456', 'Gianluigi', 'Blythe', 'K', '2019-03-23 12:31:51', 'u', 'YVvQnsUub4h', 'Gunsissa'),
+('lbaxter@optonline.net', '9597033239', 'Lakshmi', 'Demir', 'E', '2019-03-13 08:54:53', 'u', 'QBpw3cz5GJk', 'GuyKing'),
+('josem@me.com', '2913128760', 'Madhavi', 'Honda', 'F', '2019-04-20 23:15:26', 'u', '6gju2Wje787', 'HeadlineSmg'),
+('janneh@yahoo.com', '3544496985', 'Merrilyn', 'Hermans', 'V', '2019-01-18 21:17:33', 'u', 'u6ddtgCRLGj', 'HelpMissing');
+
+INSERT INTO PROFILE (Email, Phone, Fname, Lname, Minit, Jdate, P_type, P_Password, Uname, Ssn) VALUES
+('fangorn@outlook.com', '9925762957', 'Margarida', 'Donohue', 'L', '2019-01-11 09:13:39', 'a', 'spSqqMrMuvj', 'Kogagro', '123121234'),
+('smcnabb@verizon.net', '2747587440', 'Sieglinde', 'Siddall', 'M', '2019-01-14 16:35:55', 'a', 'BEgH2uFgUtS', 'Primedyst','998778987'),
+('ribet@msn.com', '2956125865', 'Primus', 'Eriksson', 'A', '2019-03-07 03:36:11', 'a', 'QrDk4dW9J4L', 'Proudmannie', '987456789'),
+('mchugh@mac.com', '6383125143', 'Dike', 'Lane', 'N', '2019-02-18 05:57:11', 'a', 'SR8XURzn99D', 'Seaguptis', '123432123'),
+('gslondon@verizon.net', '6413845245', 'Bruno', 'Cruz', 'B', '2019-02-17 18:54:29', 'a', 'pFdq3C4fMCr', 'Shockward', '678431234'),
+('shaffei@sbcglobal.net', '9475780909', 'Snezhana', 'Foster', 'B', '2019-04-14 07:24:05', 'a', 'GqravqbKtHG', 'Stylishco', '123121234');
+
+INSERT INTO ANIMAL (Added_by, A_date, Name,A_Type,Sex,Color,Diet,Available,Weight,Height,DoB,is_kid_friendly,is_vac,Description,Breed) VALUES 
+(45,'2018-09-22', 'Saul', 'S', 'M', 'Green', 'small mammals', 1, '34', '7.8', '2010-11-24',  0, 1, "Saul is a deadly snake", "Rattle"),
+(48,'2018-11-24', 'Kobe', 'S', 'M', 'Black', 'small mammals', 1, '3.5', '2.5', '2015-06-06', 0, 0, "Not the NBA player", "B. Mamba"),
+(49, '2019-04-20', 'Tamanna', 'F', 'F', 'Grey', 'other fish', 1, '120', '6', '2013-09-15', 0, 0, "Hammer Head shark that has 360 degree vision", "H. Shark"),
+(50, '2011-04-29','Angel', 'F', 'M', 'Grey/White', 'other fish', 1, '170', '6', '2012-12-11', 0, 0, "King of the sea", "G.W. Shark"),
+(45,'2015-09-22','Steven', 'I', 'M','Green','small insects',1,'.2','.2','2019-03-12', 1, 0, 'Steven the praymantis is looking for a home', 'Pray mantis'),
+(47,'2015-10-22','Charlotte', 'I','F','Black/Red', 'small insects',1, '.05','.5', '2018-12-03', 0, 0, 'Charlotte the black widow needs a home', 'B. Widow'),
+(49,'2017-09-21','Rex','D','M','Golden','Dry food', 1, '50.7', '19.5', '2012-07-01', 1, 1, 'friendly face', 'Pit Bull Terrier'),
+(48,'2015-09-22','Bailey','D','F','Black','Dry food', 1, '60.3', '22.3', '2015-12-12', 1, 1, 'friendly face', 'Labrador/Retriever'),
+(47,'2016-12-13','Charlie','B','M','Black','Grain,Seeds,Fruit',1,'8.2','24','2016-10-23',1,1,'','Black Australorp'),
+(50, '2018-04-20','Ricky','B','M','Gray/White','Nuts,Flowers,Seeds,Fruit,Insects',1,'3.2','12','2013-2-17',1,1,'','Cockatiel Parrot'),
+(47,'2018-04-21','Bella','C','F','Black/White Tuxedo','Dry Food',1,'8.2','9.1','2015-03-12',1,1,'','Tuxedo/Short Hair'),
+(48,'2018-04-22','Leo','C','M','Orange/White','Dry food',1,'8.7','9.6','2017-06-19',1,1,'','Short Hair'),
+(45,'2017-09-21','Serendipity','H','F','Bay','Grass,Hay,Oats/Wheat/Barley',1,'503','48','2014-07-12',1,1,'','Welsh Pony'),
+(46,'2018-05-12','Kaslin','H','M','Chestnut/Sorrel','Grass,Hay,Oats/Wheat/Barley',1,'510','49','2012-06-21',1,1,'','Welsh/Paint Pony'),
+(48,'2018-12-12','Toby','D','F','Black/Tan/Brown','Dry food',1,'70.7','25.1','2017-07-22',1,1,'','German Shepherd'),
+(49,'2018-07-04','Rocky','D','M','White/Brown','Dry food',1,'85.3','24.1','2016-11-27',1,1,'','American Bulldog'),
+(50,'2019-03-04','Regina','H','F','Grullo','Grass,Hay,Oats/Wheat/Barley',1,'1117','61.2','2015-01-24',1,1,'','Quarter Horse'),
+(50,'2017-07-12','Ranger','H','M','Chestnut/Sorrel','Grass,Hay,Oats/Wheat/Barley',1,'1024','59.3','2011-04-21',1,1,'','Quarter Horse'),
+(49,'2017-09-12','Lucy','C','F','Gray/Blue/Silver','Dry Food',1,'8.4','9.3','2014-02-16',1,1,'','Siamese/Tabby'),
+(45,'2017-01-21','Charlie','C','M','Black/White Tuxedo','Dry Food',1,'8.3','9.4','2015-07-22',1,0,'','Short Hair');
+
+INSERT INTO snake VALUES
+(1, 1, '7.8'),
+(2, 1, '2.5');
+
+INSERT INTO fish VALUES
+(3, 's', '1000'),
+(4, 's', '1000');
+
+INSERT INTO insect VALUES
+(5, 0, 0),
+(6, 1, 0);
+
+INSERT INTO dog VALUES
+(7, 0),
+(8, 1),
+(15, 0),
+(16, 1);
+
+INSERT INTO bird VALUES
+(9,'13',1 ),
+(10, '17', 0);
+
+INSERT INTO cat VALUES
+(11, 1, 0),
+(12, 1, 0),
+(19, 0,0),
+(20, 1, 0);
+
+INSERT INTO horse VALUES
+(13,1, '13'),
+(14, 1,'22'),
+(17, 1, '10'),
+(18, 1, '17');
+
+INSERT INTO donates_to (AID, PID, Amount, D_date) VALUES
+(1, 20, 12.25,'2019-04-12'),
+(1, 19, 20.00, '2019-03-12'),
+(2, 18, 50.00, '2019-03-01'),
+(2, 17, 43.12, '2019-02-12'),
+(3, 16, 5.00, '2019-01-01'),
+(3, 15, 12.00, '2019-02-23'),
+(4, 14, 19.25, '2019-04-20'),
+(4, 13, 17.00, '2019-02-02'),
+(5, 12, 15.00, '2019-02-21'),
+(5, 11, 18.00, '2019-03-12'),
+(6, 10, 20.00, '2019-02-10'),
+(6, 9, 1.00, '2019-03-12'),
+(7, 8, 12.23, '2018-01-12'),
+(7, 7, 25.00, '2019-03-18'),
+(8, 6, 4.00, '2019-02-12'),
+(8,5, 6.00, '2019-04-01'),
+(9,4, 8.00, '2019-03-12'),
+(9,3, 9.99, '2019-01-12'),
+(10,2, 17.00, '2019-02-04'),
+(10,1, 200.00, '2019-04-13'),
+(11,2, 13.27, '2019-02-14'),
+(11,3, 19.99, '2019-02-14'),
+(12,4, 15.00, '2019-02-14'),
+(12,5, 45.50, '2019-02-14'),
+(13,6, 20.11, '2019-02-14'),
+(13,7, 15.67,'2019-02-14'),
+(14,8, 10.00, '2019-01-01'),
+(14,9, 18.90, '2019-02-01'),
+(15,10, 5.00, '2019-03-23'),
+(15,11, 1.00, '2019-04-14'),
+(16,12, 0.01, '2019-04-01'),
+(16,13,0.01, '2019-04-01'),
+(17,14, 0.01, '2019-04-01'),
+(17,15, 0.02, '2019-04-01'),
+(18,16, 0.03, '2019-04-01'),
+(18,17, 500.00, '2019-04-01'),
+(19,18, 5.00, '2019-04-23'),
+(19,19, 20.00, '2019-05-01'),
+(20,20, 10.50,'2019-05-02'),
+(20,1, 11.35, '2019-02-10');
+
+INSERT INTO inquiry (U_PID, AID, u_date, u_q) VALUES
+(20, 20, '2019-02-10', 'Is this cat a rescue?'),
+(1, 1, '2019-04-20', 'Is this snake safe to keep with other snakes?'),
+(1, 17, '2019-03-12', 'What time do you guys open?'),
+(2, 3, '2019-02-11', 'Is this shark raised in sanctuary or in wild?'),
+(3, 4, '2019-02-10', 'Why do you guys even take these sharks?'),
+(2, 5, '2019-01-09', 'Can I take him to church?'),
+(30, 16, '2019-01-13', 'Is he a good boy?'),
+(20, 12, '2019-02-22', 'Is this cat a rescue?'),
+(20, 3, '2019-02-14', 'Will this make a good valentines gift?'),
+(20, 14, '2019-02-14', 'Will this make a good valentines gift?'),
+(20, 15, '2019-02-14', 'Will this make a good valentines gift?');
+
+INSERT INTO ADOPTION VALUES
+(1,1,'2019-02-14', 125.00),
+(2,2,'2019-03-14', 25.00),
+(4,11,'2019-02-21', 165.00),
+(2,12,'2019-01-12', 125.00),
+(1,5,'2019-02-12', 125.00),
+(5,6,'2019-04-20', 25.00),
+(1,7,'2019-04-21', 12.00),
+(6,8,'2019-05-01', 100.00),
+(2,3,'2019-05-02', 500.00),
+(14,4,'2019-05-03', 1250.00);
+
+update ANIMAL set Available = 0 where AID = 1;
+update ANIMAL set Available = 0 where AID = 2;
+update ANIMAL set Available = 0 where AID = 11;
+update ANIMAL set Available = 0 where AID = 12;
+update ANIMAL set Available = 0 where AID = 5;
+update ANIMAL set Available = 0 where AID = 6;
+update ANIMAL set Available = 0 where AID = 7;
+update ANIMAL set Available = 0 where AID = 8;
+update ANIMAL set Available = 0 where AID = 3;
+update ANIMAL set Available = 0 where AID = 4;
+
