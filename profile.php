e8a0665034fcb20cc0f2531352245d92225ae85d
Raul Jimenez <officialrauljimenez@gmail.com>
2019-04-26 12:46:30 -0500

    added profile display of adoption and donations

--- profile.php
+++ profile.php
@@ -7,30 +7,26 @@ $conn = OpenCon();
 if (empty($_SESSION['username'])) {
     header('Location: login.php');
 }
-if ($_SERVER["REQUEST_METHOD"] == "POST")
-{
-    $ssn = $_POST['ssn'];
-    $username = $_SESSION['username'];
-    $password = $_SESSION['password'];
-    $sql = "SELECT PID FROM PROFILE WHERE Uname = '$username' AND P_Password = '$password';";
-    $new_result = $conn->query($sql);
-    $row = $new_result->fetch_assoc();
-    $pid = $row['PID'];
-    if($_SESSION['p_type'] == 'u')
-    {
-        //make user a admin
-        $conn->query("UPDATE PROFILE SET P_TYPE = 'a', Ssn = '$ssn' WHERE PID = '$pid';");
-        $_SESSION['ssn'] = $ssn;
-        $_SESSION['p_type'] = 'a';
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $ssn = $_POST['ssn'];
+        $username = $_SESSION['username'];
+        $password = $_SESSION['password'];
+        $sql = "SELECT PID FROM PROFILE WHERE Uname = '$username' AND P_Password = '$password';";
+        $new_result = $conn->query($sql);
+        $row = $new_result->fetch_assoc();
+        $pid = $row['PID'];
+        if ($_SESSION['p_type'] == 'u') {
+                //make user a admin
+                $conn->query("UPDATE PROFILE SET P_TYPE = 'a', Ssn = '$ssn' WHERE PID = '$pid';");
+                $_SESSION['ssn'] = $ssn;
+                $_SESSION['p_type'] = 'a';
+            } else if ($_SESSION['p_type'] == 'a') {
+                //make an admin a user
+                $conn->query("UPDATE PROFILE SET P_TYPE = 'u', Ssn = NULL WHERE PID = '$pid';");
+                $_SESSION['ssn'] = NULL;
+                $_SESSION['p_type'] = 'u';
+            }
     }
-    else if ($_SESSION['p_type'] == 'a')
-    {
-        //make an admin a user
-        $conn->query("UPDATE PROFILE SET P_TYPE = 'u', Ssn = NULL WHERE PID = '$pid';");
-        $_SESSION['ssn'] = NULL;
-        $_SESSION['p_type'] = 'u';
-    }
-}
 $conn->close();
 ?>
 <!DOCTYPE HTML>
@@ -39,7 +35,7 @@ $conn->close();
 <head>
     <title>
         <?php
-            echo $_SESSION['username'] . '\'s profile';
+        echo $_SESSION['username'] . '\'s profile';
         ?>
     </title>
     <meta charset="utf-8" />
@@ -91,38 +87,38 @@ $conn->close();
                                 <header>
                                     <h3>Request Upgrade to Admin</h3>
                                 </header>
+                                <?php
+                                if ($_SESSION['p_type'] == 'u')
+                                    echo "Want to upgrade your account to be an admin? Click the button and enter your SSN.";
+                                else
+                                    echo "Want to become a User again?";
+                                ?>
+                                <form method="post" action="#">
+                                    <div class="row gtr-50">
                                         <?php
-                                            if($_SESSION['p_type'] == 'u')
-                                                echo "Want to upgrade your account to be an admin? Click the button and enter your SSN.";
-                                            else
-                                                echo "Want to become a User again?";
-                                        ?>
-                                        <form method="post" action="#">
-                                            <div class="row gtr-50">
-                                                <?php
-                                                if($_SESSION['p_type'] == 'u')
-                                                    echo '<div class="col-12" style="text-align:center;">
+                                        if ($_SESSION['p_type'] == 'u')
+                                            echo '<div class="col-12" style="text-align:center;">
                                                             <input name="ssn" placeholder="SSN" type="text" maxlength="9" minlength="9" />
                                                          </div>';
-                                                ?>
-                                                <div class="col-12">
-                                                <a href="#." class="image featured"><img src="images/business.jpg" alt="" /></a>
-                                                    <ul class="actions" style="text-align:center;">
-                                                        <?php
-                                                        if($_SESSION['p_type'] == 'u')
-                                                            echo '<li><input type="Submit" value="Request" n/></li>';
-                                                        else
-                                                            echo '<li><input type="Submit" value="Go back" /></li>'
+                                        ?>
+                                        <div class="col-12">
+                                            <a href="#." class="image featured"><img src="images/business.jpg" alt="" /></a>
+                                            <ul class="actions" style="text-align:center;">
+                                                <?php
+                                                if ($_SESSION['p_type'] == 'u')
+                                                    echo '<li><input type="Submit" value="Request" n/></li>';
+                                                else
+                                                    echo '<li><input type="Submit" value="Go back" /></li>'
 
-                                                        ?>
-                                                    </div>
-                                                    <div style="color:red;">NOTE: YOU WILL NOT BE ABLE TO ADOPT, DONATE, OR LIKE ANIMALS IN THE FUTURE</div>
-                                        </form>
-                                    </section>
-                                    <?php
-                                    //If admin is a user they should be able to add animals
-                                    if($_SESSION['p_type'] == 'a'){
-                                    echo '<section>
+                                                    ?>
+                                        </div>
+                                        <div style="color:red;">NOTE: YOU WILL NOT BE ABLE TO ADOPT, DONATE, OR LIKE ANIMALS IN THE FUTURE</div>
+                                </form>
+                            </section>
+                            <?php
+                            //If admin is a user they should be able to add animals
+                            if ($_SESSION['p_type'] == 'a') {
+                                echo '<section>
 											<a href="add.php" class="image featured"><img src="images/dolphin.jpg" alt="" /></a>
 											<header>
 												<h3>Add a Pet</h3>
@@ -143,10 +139,8 @@ $conn->close();
 											</ul>
                                         </section>
                                     ';
-                                    }
-                                    else if($_SESSION['p_type'] == 'u')
-                                    {
-                                        echo '<section>
+                            } else if ($_SESSION['p_type'] == 'u') {
+                                    echo '<section>
 											<a href="adopt.php" class="image featured"><img src="images/adopt.jpg" alt="" /></a>
 											<header>
 												<h3>Adopt a Pet</h3>
@@ -156,9 +150,9 @@ $conn->close();
 												<li><a href="adopt.php" class="button">Adopt</a></li>
 											</ul>
                                         </section>';
-                                    }
-                                    ?>
-                            </section>
+                                }
+                            ?>
+                        </section>
                         </section>
 
                     </div>
@@ -169,7 +163,7 @@ $conn->close();
                             <header>
                                 <h2>
                                     <?php
-                                        echo "Hello " . $_SESSION['username']
+                                    echo "Hello " . $_SESSION['username']
                                     ?>
                                 </h2>
                                 <p>This information is read-only for your purposes. It is recommended you take a picture of your username and password.</p>
@@ -177,18 +171,73 @@ $conn->close();
                             <a href="#" class="image featured"><img src="images/lock.jpg" alt="" /></a>
                             <p>
                                 <table style="border:1px solid black;">
-                                    <tr><th colspan="2">Information about you</th></tr>
-                                <?php
+                                    <tr>
+                                        <th colspan="2">Information about you</th>
+                                    </tr>
+                                    <?php
                                     echo "<tr> <td>Full Name</td>" . "<td>" . $_SESSION['fname'] . " " . $_SESSION['minit'] . ". " . $_SESSION['lname'] . "</td></tr>";
-                                    echo "<tr> <td>Phone Number</td>" . "<td>" . "(" . substr($_SESSION['phone'], 0, 3) . ") - " . substr($_SESSION['phone'], 3,3). " - " . substr($_SESSION['phone'], 6, 4) ."</td>";
+                                    echo "<tr> <td>Phone Number</td>" . "<td>" . "(" . substr($_SESSION['phone'], 0, 3) . ") - " . substr($_SESSION['phone'], 3, 3) . " - " . substr($_SESSION['phone'], 6, 4) . "</td>";
                                     echo "<tr> <td>Email</td>" . "<td>" . $_SESSION['email'] . "</td>";
                                     echo "<tr> <td>Username</td>" . "<td>"  . $_SESSION['username'] . "</td>";
                                     echo "<tr> <td>Password</td>" . "<td>" . $_SESSION['password'] . "</td>";
-                                    if($_SESSION['p_type'] == 'a')
+                                    if ($_SESSION['p_type'] == 'a')
                                         echo "<tr> <td>Account Type</td>" . "<td> Admin </td>";
-                                    else 
-                                        echo "<tr> <td>Account Type</td>" . "<td> User </td>";                                    
-                                ?>
+                                    else
+                                        echo "<tr> <td>Account Type</td>" . "<td> User </td>";
+                                    ?>
+                                </table>
+                            </p>
+                            <p>
+                                <table style="border:1px solid black;width:100%;">
+                                    <tr style="border:1px solid black;">
+                                        <th colspan="3">Adopted Pets</th>
+                                    </tr>
+                                    <tr>
+                                        <th colspan="1" style="border:1px solid black;">Name</th>
+                                        <th colspan="1" style="border:1px solid black;">Date</th>
+                                        <th colspan="1" style="border:1px solid black;">Fee</th>
+                                    </tr>
+                                    <?php
+                                    $conn = OpenCon();
+                                    $n_pid = $_SESSION['pid'];
+                                    $get_adoptions_sql = "SELECT * FROM adoption where PID=$n_pid;";
+                                    $adoption_aid_arr = $conn->query($get_adoptions_sql);
+                                    while ($adoption_aid = $adoption_aid_arr->fetch_assoc()) {
+                                            $new_aid = $adoption_aid['AID'];
+                                            $names = $conn->query("SELECT * from animal where AID=$new_aid;");
+                                            $name = $names->fetch_assoc();
+                                            echo "<tr style=\"text-align:center;\"><td>" . $name['Name'] . "</td>";
+                                            echo "<td>" . $adoption_aid['adopt_date'] . "</td>";
+                                            echo "<td>" . "\$" . number_format($adoption_aid['fee'], 2) . "</td></tr>";
+                                        }
+                                    $conn->close();
+                                    ?>
+                                </table>
+                            
+                                <table style="border:1px solid black;width:100%;">
+                                    <tr style="border:1px solid black;">
+                                        <th colspan="3">Donations</th>
+                                    </tr>
+                                    <tr>
+                                        <th colspan="1" style="border:1px solid black;">Name</th>
+                                        <th colspan="1" style="border:1px solid black;">Date</th>
+                                        <th colspan="1" style="border:1px solid black;">Amount</th>
+                                    </tr>
+                                    <?php                         
+                                    $conn = OpenCon();
+                                    $new_pid = $_SESSION['pid'];
+                                    $get_donations_sql = "SELECT * FROM donates_to where PID=$new_pid;";
+                                    $donations_arr = $conn->query($get_donations_sql);
+                                    while ($donation_aid = $donations_arr->fetch_assoc()) {
+                                            $n_aid = $donation_aid['AID'];
+                                            $names_d = $conn->query("SELECT * from animal where AID=$n_aid;");
+                                            $name_d = $names_d->fetch_assoc();
+                                            echo "<tr style=\"text-align:center;\"><td>" . $name_d['Name'] . "</td>";
+                                            echo "<td>" . $donation_aid['D_date'] . "</td>";
+                                            echo "<td>" . "\$" . number_format($donation_aid['Amount'], 2) . "</td></tr>";
+                                        }
+                                    $conn->close();
+                                    ?>
                                 </table>
                             </p>
                         </article>
